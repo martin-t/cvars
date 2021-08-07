3d12cbb8e944bfa3ed891fde19ea59ba3a42c607
Martin Taibr <taibr.martin@gmail.com>
2021-08-07 08:35:35 +0200

    Comment

--- tests/fnlike/test_extra_impl.rs
+++ tests/fnlike/test_extra_impl.rs
@@ -1,9 +1,11 @@
+// Make sure users can add their own impl bloks to the generated struct.
+
 mod shared;
 
 use shared::Cvars;
 
 impl Cvars {
-    fn extra_method() {}
+    fn extra_method(&self) {}
 }
 
 fn main() {
