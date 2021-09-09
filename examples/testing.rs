13999d4f0c8ba3db44409016010bdc423a9f737b
Martin Taibr <taibr.martin@gmail.com>
2021-09-09 13:33:59 +0200

    update testing

--- examples/testing.rs
+++ examples/testing.rs
@@ -16,6 +16,10 @@ pub struct Cvars {
     pub g_float: f32,
     pub g_double: f64,
     pub g_enum: Enum,
+    #[warn(clippy::pedantic)] // Testing that the field can have other attributes
+    #[cvars(skip)]
+    #[allow(clippy::pedantic)]
+    pub g_skipped: i32,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Display, EnumString)]
