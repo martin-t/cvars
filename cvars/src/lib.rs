b10438eef2d8eade02339ec277bf4bae6866063e
Martin Taibr <taibr.martin@gmail.com>
2023-01-10 13:42:54 +0100

    Mv

---
+++ cvars/src/lib.rs
@@ -0,0 +1,20 @@
+//! TODO
+
+//#![warn(missing_docs)] TODO
+
+//#![doc = include_str!("../README.md")] TODO
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
