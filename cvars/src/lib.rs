1c68a4c7e529495f672200d79ed41eea37f276ad
Martin Taibr <taibr.martin@gmail.com>
2023-01-15 13:18:55 +0100

    Note

--- cvars/src/lib.rs
+++ cvars/src/lib.rs
@@ -6,6 +6,9 @@ pub use cvars_macros::{SetGet, SetGetDummy, cvars};
 
 /// A trait for writing generic code
 /// that can access cvars but doesn't know the concrete Cvars struct.
+///
+/// Implementation note: This trait can't include the `get` and `set` methods
+/// because it would no longer be object-safe.
 pub trait SetGet {
     fn get_string(&self, cvar_name: &str) -> Result<String, String>;
     fn set_str(&mut self, cvar_name: &str, str_value: &str) -> Result<(), String>;
