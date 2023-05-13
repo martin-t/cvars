cb7a713695abdb2754e5ab45990d0afacae285c0
Martin Taibr <taibr.martin@gmail.com>
2023-05-13 03:04:57 +0200

    Reenable all benches

--- cvars-test-compile-time/bench.sh
+++ cvars-test-compile-time/bench.sh
@@ -3,12 +3,12 @@
 # Exit on error, unset variables and pipeline errors
 set -euo pipefail
 
-# hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features nomacro"
-# hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive-dummy,cvars-100"
-# hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive-dummy,cvars-1000"
-# hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive-dummy,cvars-10000"
-# hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive,cvars-100"
-# hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive,cvars-1000"
+hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features nomacro"
+hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive-dummy,cvars-100"
+hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive-dummy,cvars-1000"
+hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive-dummy,cvars-10000"
+hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive,cvars-100"
+hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive,cvars-1000"
 #hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features derive,cvars-10000" # TODO this gets stuck
 hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features fnlike,cvars-100"
 hyperfine --warmup 2 "echo '// test' >> src/main.rs && time cargo build --features fnlike,cvars-1000"
