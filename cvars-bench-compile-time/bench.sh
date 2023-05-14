9e8eb3dc57c74c12fe3ada8d5bb5b226bdc088e6
Martin Taibr <taibr.martin@gmail.com>
2023-05-14 17:20:11 +0200

    Comment

--- cvars-bench-compile-time/bench.sh
+++ cvars-bench-compile-time/bench.sh
@@ -8,6 +8,9 @@ set -euo pipefail
 # Run it outside hyperfine so we can see progress.
 cargo build --features nomacro,cvars-100
 
+# Hyperfine supports multiple benchmarks in one command and a cleanup command
+# but let's keep everything separate so we can comment some benches out
+# and so we can easily copy-paste the commands to run them manually.
 hyperfine --warmup 2 "echo '// test' >> src/main.rs && cargo build --features nomacro,cvars-100"
 hyperfine --warmup 2 "echo '// test' >> src/main.rs && cargo build --features nomacro,cvars-1000"
 hyperfine --warmup 2 "echo '// test' >> src/main.rs && cargo build --features nomacro,cvars-10000"
