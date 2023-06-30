118b307a903b10308d7566b414c91ec58c98600b
Martin Taibr <taibr.martin@gmail.com>
2023-06-30 22:48:12 +0200

    Simplify tests

--- cvars-macros/tests/progress.rs
+++ cvars-macros/tests/progress.rs
@@ -1,5 +1,7 @@
 use std::fs;
 
+use trybuild::TestCases;
+
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
@@ -8,22 +10,8 @@ fn tests() {
 
     // Run all test_* files for both derive and fnlike macros.
     // The files need to be copied to different directories because of how trybuild works.
-    fs::create_dir_all("tests/derive").unwrap();
-    fs::copy("tests/shared_derive.rs", "tests/derive/shared.rs").unwrap();
-    for entry in fs::read_dir("tests/shared").unwrap() {
-        let path_src = entry.unwrap().path();
-        let path_dest = path_src.to_str().unwrap().replace("shared", "derive");
-        fs::copy(&path_src, &path_dest).unwrap();
-        t.pass(&path_dest);
-    }
-    fs::create_dir_all("tests/fnlike").unwrap();
-    fs::copy("tests/shared_fnlike.rs", "tests/fnlike/shared.rs").unwrap();
-    for entry in fs::read_dir("tests/shared").unwrap() {
-        let path_src = entry.unwrap().path();
-        let path_dest = path_src.to_str().unwrap().replace("shared", "fnlike");
-        fs::copy(&path_src, &path_dest).unwrap();
-        t.pass(&path_dest);
-    }
+    run_shared_tests("derive", &t);
+    run_shared_tests("fnlike", &t);
 
     drop(t);
 
@@ -33,4 +21,19 @@ fn tests() {
     // Doesn't run if tests fail (TestCases::drop panics).
     fs::remove_dir_all("tests/derive").unwrap();
     fs::remove_dir_all("tests/fnlike").unwrap();
+}
+
+fn run_shared_tests(kind: &str, t: &TestCases) {
+    fs::create_dir_all(format!("tests/{kind}")).unwrap();
+    fs::copy(
+        format!("tests/shared_{kind}.rs"),
+        format!("tests/{kind}/shared.rs"),
+    )
+    .unwrap();
+    for entry in fs::read_dir("tests/shared").unwrap() {
+        let path_src = entry.unwrap().path();
+        let path_dest = path_src.to_str().unwrap().replace("shared", kind);
+        fs::copy(&path_src, &path_dest).unwrap();
+        t.pass(&path_dest);
+    }
 }
